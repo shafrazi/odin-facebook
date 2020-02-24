0e61819f82b422166375f881bd8bd3c41efb102f
shafrazi <shafrazi@live.com>
2020-02-25 01:05:29 +0530

    ajax likes/unlikes implemented

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,17 +1,23 @@
 class LikesController < ApplicationController
   def create
-    post = Post.find(params[:post_id])
-    unless current_user.liked?(post)
-      @like = post.likes.build(user_id: current_user.id)
-      if @like.save
-        redirect_to request.referrer
+    @post = Post.find(params[:post_id])
+    unless current_user.liked?(@post)
+      @like = @post.likes.build(user_id: current_user.id)
+      @like.save
+      respond_to do |format|
+        format.html { redirect_to request.referrer }
+        format.js
       end
     end
   end
 
   def destroy
     @like = Like.find(params[:id])
+    @post = @like.post
     @like.destroy
-    redirect_to request.referrer
+    respond_to do |format|
+      format.html { redirect_to request.referrer }
+      format.js
+    end
   end
 end
