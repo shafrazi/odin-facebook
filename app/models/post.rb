225cf398007e6850711ee9708cfbf69e7a4d7955
shafrazi <shafrazi@live.com>
2020-02-26 00:18:04 +0530

    minor improvements

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   validates :content, presence: true
   belongs_to :user
   has_many :comments, dependent: :destroy
-  has_many :likes
+  has_many :likes, dependent: :destroy
   accepts_nested_attributes_for :comments
 end
